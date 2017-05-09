83a0a7ee7d8c42cfa14ea12ea8af929d97e1dd25
Karol Sluszniak <ksluszniak@gmail.com>
2017-05-09 14:33:38 +0200

    Configure Hex package

--- mix.exs
+++ mix.exs
@@ -7,15 +7,22 @@ defmodule Surgex.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()] ++ ex_doc_project()
-  end
-
-  def ex_doc_project do
-    [name: "Surgex",
+     deps: deps(),
+     package: package(),
+     name: "Surgex",
+     description: "All Things Elixir @ Surge Ventures",
      source_url: "https://github.com/surgeventures/surgex",
      homepage_url: "https://github.com/surgeventures/surgex",
      docs: [main: "README",
+            logo: "logo.png",
             extras: ["README.md"]]]
+  end
+
+  defp package do
+    [maintainers: ["Karol Słuszniak"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/surgeventures/surgex"},
+     files: ~w(lib LICENSE.md mix.exs README.md)]
   end
 
   def application do
