a5743cfe119260c473c7d7c18d11021972e12838
Kamil Pluta <kamil.p@shedul.com>
2017-07-04 12:33:23 +0200

    Apply review comments

--- lib/surgex/parser/parsers/contain_parser.ex
+++ lib/surgex/parser/parsers/contain_parser.ex
@@ -1,15 +1,14 @@
 defmodule Surgex.Parser.ContainParser do
   @moduledoc """
-  Checks if the given parameter's value is on the list of allowed values
+  Checks if the given parameter's value is on the list of allowed values.
   """
 
-  def call(nil, _allowed_values), do: {:error, :value_is_not_allowed}
-  def call(_, []), do: {:error, :value_is_not_allowed}
+  def call(nil, _allowed_values), do: {:ok, nil}
   def call(input, allowed_values) when is_list(allowed_values) do
     case Enum.member?(allowed_values, input) do
       true -> {:ok, input}
-      false -> {:error, :value_is_not_allowed}
+      false -> {:error, :invalid_value}
     end
   end
-  def call(_input, _allowed_values), do: {:error, :allowed_values_is_not_a_list}
+  def call(_input, _allowed_values), do: {:error, :invalid_value}
 end
--- test/parser/parsers/contain_parser_test.exs
+++ test/parser/parsers/contain_parser_test.exs
@@ -3,15 +3,15 @@ defmodule Surgex.Parser.ContainParserTest do
   alias Surgex.Parser.ContainParser
 
   test "nil" do
-    assert ContainParser.call(nil, ["allowed value"]) == {:error, :value_is_not_allowed}
+    assert ContainParser.call(nil, ["allowed value"]) == {:ok, nil}
   end
 
   test "empty array of allowed values" do
-    assert ContainParser.call("value", []) == {:error, :value_is_not_allowed}
+    assert ContainParser.call("value", []) == {:error, :invalid_value}
   end
 
   test "allowed values not a list" do
-    assert ContainParser.call("value", "not a list") == {:error, :allowed_values_is_not_a_list}
+    assert ContainParser.call("value", "not a list") == {:error, :invalid_value}
   end
 
   test "valid input is on the list of allowed values" do
@@ -19,6 +19,6 @@ defmodule Surgex.Parser.ContainParserTest do
   end
 
   test "valid input is not on the list of allowed values" do
-    assert ContainParser.call("value", ["different_value"]) == {:error, :value_is_not_allowed}
+    assert ContainParser.call("value", ["different_value"]) == {:error, :invalid_value}
   end
 end
