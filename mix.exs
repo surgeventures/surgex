b77bb05ded7b88226104aec6535320cfb3de2abd
Karol Sluszniak <ksluszniak@gmail.com>
2017-07-04 13:07:40 +0200

    0.5.0: added contain parser

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Surgex.Mixfile do
 
   def project do
     [app: :surgex,
-     version: "0.4.2",
+     version: "0.5.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
