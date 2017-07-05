1312eff3551a97231c36b6a1bbbd6842a60230ec
Karol Sluszniak <ksluszniak@gmail.com>
2017-07-05 12:43:00 +0200

    Cover new Config module with 100% tests

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,9 @@ use Mix.Config
 config :surgex, :config_test,
   filled_key: "filled value",
   system_key_without_default: {:system, "NON_EXISTING_ENV_VAR"},
-  system_key_with_default: {:system, "NON_EXISTING_ENV_VAR", "default value"}
+  system_key_with_default: {:system, "NON_EXISTING_ENV_VAR", default: "default value"},
+  system_key_with_boolean_type: {:system, "BOOLEAN_ENV_VAR", type: :boolean},
+  system_key_with_integer_type: {:system, "INTEGER_ENV_VAR", type: :integer}
 
 config :surgex, flat_config_key: "flat value"
 
