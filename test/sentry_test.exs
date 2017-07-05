5c41fb154272924d9b1f5de26a2457d5e3e3b826
Karol Sluszniak <ksluszniak@gmail.com>
2017-07-05 15:28:28 +0200

    Add sentry test

---
+++ test/sentry_test.exs
@@ -0,0 +1,76 @@
+defmodule Surgex.SentryTest do
+  use ExUnit.Case
+  import ExUnit.CaptureLog
+
+  describe "init/0" do
+    test "patch enabled" do
+      Mix.Config.persist(surgex: [
+        sentry_patch_enabled: true
+      ])
+
+      log = capture_log(fn ->
+        Surgex.Sentry.init()
+      end)
+
+      version = Mix.Project.config[:version]
+
+      assert log =~
+        ~s{Patching Sentry config (environment: :test, release: "#{version}")}
+
+      Mix.Config.persist(surgex: [
+        sentry_patch_enabled: true,
+        sentry_environment: "abc",
+        sentry_release: "def",
+      ])
+
+      log = capture_log(fn ->
+        Surgex.Sentry.init()
+      end)
+
+      assert log =~
+        ~s{Patching Sentry config (environment: "abc", release: "def")}
+    end
+  end
+
+  describe "scrub_params/1" do
+    test "params with secrets" do
+      assert Surgex.Sentry.scrub_params(%{
+        params: %{
+          "username" => "a",
+          "password" => "secret",
+          "deep_array" => [
+            %{
+              "username" => "b",
+              "password" => "secret",
+            },
+            %{
+              "username" => "c",
+              "password" => "secret",
+            }
+          ],
+          "deep_map" => %{
+            "username" => "d",
+            "password" => "secret",
+          }
+        }
+      }) == %{
+        "deep_array" => [
+          %{
+            "password" => "[Filtered]",
+            "username" => "b"
+          },
+          %{
+            "password" => "[Filtered]",
+            "username" => "c"
+          }
+        ],
+        "deep_map" => %{
+          "password" => "[Filtered]",
+          "username" => "d"
+        },
+        "password" => "[Filtered]",
+        "username" => "a"
+      }
+    end
+  end
+end
