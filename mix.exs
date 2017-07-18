8e03dd914c1bf44be741adf1529d2e8fbfcb2d15
Karol Sluszniak <ksluszniak@gmail.com>
2017-07-18 10:27:53 +0200

    Update version to 2.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Surgex.Mixfile do
 
   def project do
     [app: :surgex,
-     version: "1.5.1",
+     version: "2.0.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
