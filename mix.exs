6abad30614990ca29c9b5431e63276fa81dc7d44
Karol Sluszniak <ksluszniak@gmail.com>
2017-07-20 12:19:05 +0200

    Release 1.6.0
    - Added Surgex.Config.Session

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Surgex.Mixfile do
 
   def project do
     [app: :surgex,
-     version: "1.5.2",
+     version: "1.6.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -53,7 +53,8 @@ defmodule Surgex.Mixfile do
      {:ex_phone_number, "~> 0.1.1", optional: true},
      {:excoveralls, "~> 0.7", only: :test},
      {:inch_ex, "~> 0.5", only: [:dev, :test]},
-     {:jabbax, ">= 0.1.0", optional: true}]
+     {:jabbax, ">= 0.1.0", optional: true},
+     {:plug, "~> 1.3.2 or ~> 1.4", optional: true}]
   end
 
   defp check_alias do
