886c8ff8502033282c63cf001debf4346949c904
Karol Sluszniak <ksluszniak@gmail.com>
2017-08-10 10:28:19 +0200

    Bump version to 2.7.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Surgex.Mixfile do
 
   def project do
     [app: :surgex,
-     version: "2.6.0",
+     version: "2.7.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
