e5739cf3d31c5ccf4f01d21cac78bae222c5a530
Konrad Kozicki <konrad@shedul.com>
2018-05-07 12:24:55 +0200

    Add trim, min, max options for StringParser

--- lib/surgex/parser/parsers/string_parser.ex
+++ lib/surgex/parser/parsers/string_parser.ex
@@ -1,7 +1,71 @@
 defmodule Surgex.Parser.StringParser do
-  @moduledoc false
+  @moduledoc """
+  Available options:
+  - **trim** is trimming whitespaces from the string, takes priority over min and max options
+  - **min** is a minimal length of the string, returns :too_short error symbol
+  - **max** is a maximal length of the string, returns :too_long error symbol
+  """
 
-  def call(nil), do: {:ok, nil}
-  def call(""), do: {:ok, nil}
-  def call(input) when is_binary(input), do: {:ok, input}
+
+  @opts [:trim, :min, :max]
+
+  def call(input, opts \\ [])
+  def call(nil, _opts), do: {:ok, nil}
+  def call(input, opts) when is_binary(input) do
+    validate_opts!(opts)
+    ordered_opts = set_opts_priority(opts)
+    input
+    |> process_opts(ordered_opts)
+    |> nullify_empty_output()
+  end
+
+  def validate_opts!(nil), do: :ok
+  def validate_opts!(opts) do
+    invalid_opts = Keyword.keys(opts) -- @opts
+    if Enum.any?(invalid_opts) do
+      raise ArgumentError, message: "opts: #{opts} are invalid for string parser"
+    end
+  end
+
+  defp set_opts_priority(opts) do
+    Enum.sort(opts, &(get_order_index(&1) <= get_order_index(&2)))
+  end
+
+  defp get_order_index({key, _value}) do
+    Enum.find_index(@opts, &(&1 == key))
+  end
+
+  defp process_opts(input, []), do: {:ok, input}
+  defp process_opts(input, [{opt, value} | remaining]) do
+    result = case opt do
+      :trim -> trim(input)
+      :min -> validate_min(input, value)
+      :max -> validate_max(input, value)
+    end
+    case result do
+      {:ok, output} -> process_opts(output, remaining)
+      {:error, error_type} -> {:error, error_type}
+    end
+  end
+
+  def trim(input), do: {:ok, String.trim(input)}
+
+  def validate_min(input, value) do
+    if String.length(input) >= value do
+      {:ok, input}
+    else
+      {:error, :too_short}
+    end
+  end
+
+  def validate_max(input, value) do
+    if String.length(input) <= value do
+      {:ok, input}
+    else
+      {:error, :too_long}
+    end
+  end
+
+  defp nullify_empty_output({:ok, ""}), do: {:ok, nil}
+  defp nullify_empty_output(output), do: output
 end
--- test/surgex/parser/parsers/string_parser_test.exs
+++ test/surgex/parser/parsers/string_parser_test.exs
@@ -10,4 +10,37 @@ defmodule Surgex.Parser.StringParserTest do
     assert StringParser.call("") == {:ok, nil}
     assert StringParser.call("abc") == {:ok, "abc"}
   end
+
+  test "min" do
+    opt = [min: 4]
+    assert StringParser.call(nil, opt) == {:ok, nil}
+    assert StringParser.call("", opt) == {:error, :too_short}
+    assert StringParser.call("abc", opt) == {:error, :too_short}
+    assert StringParser.call("abcd", opt) == {:ok, "abcd"}
+    assert StringParser.call("abcde", opt) == {:ok, "abcde"}
+  end
+
+  test "max" do
+    opt = [max: 4]
+    assert StringParser.call(nil, opt) == {:ok, nil}
+    assert StringParser.call("", opt) == {:ok, nil}
+    assert StringParser.call("abc", opt) == {:ok, "abc"}
+    assert StringParser.call("abcd", opt) == {:ok, "abcd"}
+    assert StringParser.call("abcde", opt) == {:error, :too_long}
+  end
+
+  test "trim" do
+    opt = [trim: true]
+    assert StringParser.call(nil, opt) == {:ok, nil}
+    assert StringParser.call("", opt) == {:ok, nil}
+    assert StringParser.call("abc", opt) == {:ok, "abc"}
+    assert StringParser.call("\t\n  abc\t  \n", opt) == {:ok, "abc"}
+  end
+
+  test "trim priority over min and max" do
+    opt = [min: 3, max: 4, trim: true]
+    assert StringParser.call("  ab  ", opt) == {:error, :too_short}
+    assert StringParser.call("  abc  ", opt) == {:ok, "abc"}
+    assert StringParser.call("  abcde  ", opt) == {:error, :too_long}
+  end
 end
