d173af3f5dd38ff25a51ce37b05fc8e2614fa1d2
Kamil Pluta <kamil.p@shedul.com>
2018-05-16 13:34:30 +0200

    Apply review suggestions

--- lib/surgex/parser/parsers/resource_array_parser.ex
+++ lib/surgex/parser/parsers/resource_array_parser.ex
@@ -3,6 +3,7 @@ defmodule Surgex.Parser.ResourceArrayParser do
 
   def call(list, item_parser, opts \\ [])
   def call(nil, _item_parser, _opts), do: {:ok, nil}
+
   def call(list, item_parser, opts) when is_list(list) do
     min = Keyword.get(opts, :min)
     max = Keyword.get(opts, :max)
@@ -14,24 +15,16 @@ defmodule Surgex.Parser.ResourceArrayParser do
     end
   end
 
-  defp validate_length(list, nil, nil), do: {:ok, list}
-  defp validate_length(list, nil, max) do
-    case length(list) > max do
-      true -> {:error, :too_long}
-      false -> {:ok, list}
-    end
-  end
-  defp validate_length(list, min, nil) do
-    case length(list) < min do
-      true -> {:error, :too_short}
-      false -> {:ok, list}
-    end
-  end
   defp validate_length(list, min, max) do
-    case {length(list) < min, length(list) > max} do
-      {true, _} -> {:error, :too_short}
-      {false, true} -> {:error, :too_long}
-      {false, false} -> {:ok, list}
+    cond do
+      min && length(list) < min ->
+        {:error, :too_short}
+
+      max && length(list) > max ->
+        {:error, :too_long}
+
+      true ->
+        {:ok, list}
     end
   end
 
@@ -46,10 +39,12 @@ defmodule Surgex.Parser.ResourceArrayParser do
   defp reduce({{:ok, result}, _index}, {output, errors}) do
     {[result | output], errors}
   end
+
   defp reduce({{:error, :invalid_pointers, pointers}, index}, {output, errors}) do
-    new_errors = Enum.map(pointers, fn {reason, pointer} ->
-      {reason, "#{index}/#{pointer}"}
-    end)
+    new_errors =
+      Enum.map(pointers, fn {reason, pointer} ->
+        {reason, "#{index}/#{pointer}"}
+      end)
 
     {output, errors ++ new_errors}
   end
