9cf66eaba0dc0938c445d10c8becb4871a355ee6
Marcin Swiatkiewicz <marcin.s@shedul.com>
2018-06-21 17:45:44 +0200

    [

---
+++ lib/surgex/repo_helpers/repo_helpers.ex
@@ -0,0 +1,35 @@
+defmodule Surgex.RepoHelpers do
+  @moduledoc """
+  Common helpers to be used in Ecto Repos
+  """
+
+  @doc """
+  Dynamically loads the repository url and connection pool size from the environment variable.
+  """
+
+  def set_db_url({:ok, opts}, db_url_env) do
+    {:ok, Keyword.put(opts, :url, System.get_env(db_url_env))}
+  end
+
+  def set_db_pool_size({:ok, opts}, db_pool_size_env) do
+    serve_endpoints = Application.get_env(:phoenix, :serve_endpoints)
+    server_pool_size = parse_server_db_pool_size(System.get_env(db_pool_size_env))
+
+    if serve_endpoints && is_integer(server_pool_size) do
+      {:ok, Keyword.put(opts, :pool_size, server_pool_size)}
+    else
+      {:ok, opts}
+    end
+  end
+
+  defp parse_server_db_pool_size(nil), do: nil
+
+  defp parse_server_db_pool_size(server_pool_size_string) do
+    case Integer.parse(server_pool_size_string) do
+      {integer, ""} -> integer
+      _ -> nil
+    end
+  end
+
+
+end
---
+++ test/surgex/repo_helpers/repo_helpers_test.exs
@@ -0,0 +1,18 @@
+defmodule Surgex.RepoHelpersTest do
+  use ExUnit.Case
+  use Plug.Test
+
+  alias Surgex.RepoHelpers
+
+  # test "set db_pool_size" do
+  #   System.put_env("TEST_DB_CONNECTION_POOL", "5")
+  #
+  #   RepoHelpers.set_db_pool_size("TEST_DB_CONNECTION_POOL")
+  # end
+  #
+  # test "set db_url" do
+  #   System.put_env("TEST_DB_URL", "p://h:p/dbn")
+  #
+  #   RepoHelpers.set_db_url("TEST_DB_URL")
+  # end
+end
