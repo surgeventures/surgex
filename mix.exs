ecce2417c346f18d3c987aba6b875239675f07ae
Konrad Kozicki <konrad@shedul.com>
2018-11-16 11:58:14 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Surgex.Mixfile do
   def project do
     [
       app: :surgex,
-      version: "2.24.1",
+      version: "2.25.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
