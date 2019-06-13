2155b1bdbc990400d4954ec178298ac37abc6124
Argonus <argonuspiotr@gmail.com>
2019-06-13 09:53:25 +0200

    Add types for geo structs

--- lib/surgex/parser/structs/geobox.ex
+++ lib/surgex/parser/structs/geobox.ex
@@ -2,6 +2,9 @@ defmodule Surgex.Parser.Geobox do
   @moduledoc """
   Holds a box made of two points on Earth's surface.
   """
+  alias Surgex.Parser.Geolocation
+
+  @type t :: %__MODULE__{north_east: Geolocation.t(), south_west: Geolocation.t()}
 
   defstruct [:north_east, :south_west]
 end
