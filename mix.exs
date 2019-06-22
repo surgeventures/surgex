60a6c219edaf778b674446330c2d2e44495b08d5
Argonus <argonuspiotr@gmail.com>
2019-06-22 13:12:51 +0200

    update deps

--- mix.exs
+++ mix.exs
@@ -62,9 +62,9 @@ defmodule Surgex.Mixfile do
   defp deps do
     [
       {:credo, "~> 0.8.1", only: [:dev, :test]},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:ex_machina, "~> 2.0", only: :test},
+      {:ex_machina, "~> 2.3", only: :test},
       {:excoveralls, "~> 0.7", only: :test},
       {:inch_ex, "~> 0.5", only: [:dev, :test]},
       {:mock, "~> 0.2.1", only: :test},
@@ -74,10 +74,10 @@ defmodule Surgex.Mixfile do
 
   defp optional_deps do
     [
-      {:appsignal, "~> 1.0"},
-      {:confix, "~> 0.4.0"},
+      {:appsignal, "~> 1.8"},
+      {:confix, "~> 0.4"},
       {:ecto_sql, "~> 3.0"},
-      {:jabbax, ">= 0.1.0"},
+      {:jabbax, "~> 0.1"},
       {:plug, "~> 1.7"}
     ]
     |> Enum.map(&merge_dep_flags(&1, optional: true))
