2a2a8f22ba3604acdf0ba8cb6963cd1d54822b3f
Kamil Kowalski <kamil@kamilkowalski.pl>
2021-02-05 18:11:46 +0100

    Fix warning in tests

--- test/surgex/parser/parsers/email_parser_test.exs
+++ test/surgex/parser/parsers/email_parser_test.exs
@@ -7,19 +7,19 @@ defmodule Surgex.Parser.EmailParserTest do
   end
 
   test "valid input" do
-    valid_emails =
-      [
-        "me@example.com",
-        "example@superlongdomainloremipsumdolor.co.uk",
-        "mailhost!username@example.org",
-        "me+you@gmail.com",
-        "other.email-with-hyphen@example.com",
-        "\".John.Doe\"@example.com",
-        "user%example.com@example.org"
-      ]
-      |> Enum.each(fn email ->
-        assert EmailParser.call(email) == {:ok, email}
-      end)
+    valid_emails = [
+      "me@example.com",
+      "example@superlongdomainloremipsumdolor.co.uk",
+      "mailhost!username@example.org",
+      "me+you@gmail.com",
+      "other.email-with-hyphen@example.com",
+      "\".John.Doe\"@example.com",
+      "user%example.com@example.org"
+    ]
+
+    Enum.each(valid_emails, fn email ->
+      assert EmailParser.call(email) == {:ok, email}
+    end)
   end
 
   test "invalid input" do
