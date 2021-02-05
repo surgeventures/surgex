cb6d57412051442ee375d6b731f2f7b1c3627462
Kamil Kowalski <kamil@kamilkowalski.pl>
2021-02-05 10:51:46 +0100

    Start Ecto repos in tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,12 @@
 Application.ensure_all_started(:ex_machina)
-Mix.EctoSQL.ensure_started(Surgex.Repo, [])
-Mix.EctoSQL.ensure_started(Surgex.ForeignRepo, [])
+
+Supervisor.start_link(
+  [
+    Surgex.Repo,
+    Surgex.ForeignRepo
+  ],
+  strategy: :one_for_one
+)
 
 ExUnit.start()
 
