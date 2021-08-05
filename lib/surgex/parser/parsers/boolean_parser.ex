5c78bcd3eeed35136a3d6f6c2436e3be3c677922
Maciej Lopatka <gmsaner@gmail.com>
2021-08-05 18:59:09 +0200

    Extend parsing of boolean and integer values

--- lib/surgex/parser/parsers/boolean_parser.ex
+++ lib/surgex/parser/parsers/boolean_parser.ex
@@ -8,6 +8,8 @@ defmodule Surgex.Parser.BooleanParser do
   def call(nil), do: {:ok, nil}
   def call("0"), do: {:ok, false}
   def call("1"), do: {:ok, true}
+  def call("false"), do: {:ok, false}
+  def call("true"), do: {:ok, true}
   def call(false), do: {:ok, false}
   def call(true), do: {:ok, true}
   def call(_input), do: {:error, :invalid_boolean}
