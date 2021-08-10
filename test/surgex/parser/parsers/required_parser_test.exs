8ea8388e7d1536d4e1e724fc5ea49cfdcfa6cbb9
Maciej Lopatka <gmsaner@gmail.com>
2021-08-10 11:11:50 +0200

    Update RequiredParser to accept an empty string as valid input

--- test/surgex/parser/parsers/required_parser_test.exs
+++ test/surgex/parser/parsers/required_parser_test.exs
@@ -4,6 +4,7 @@ defmodule Surgex.Parser.RequiredParserTest do
 
   test "valid input" do
     assert RequiredParser.call(123) == {:ok, 123}
+    assert RequiredParser.call("") == {:ok, ""}
     assert RequiredParser.call("abc") == {:ok, "abc"}
     assert RequiredParser.call(1.23) == {:ok, 1.23}
     assert RequiredParser.call(%{}) == {:ok, %{}}
@@ -11,7 +12,6 @@ defmodule Surgex.Parser.RequiredParserTest do
 
   test "invalid input" do
     assert RequiredParser.call(nil) == {:error, :required}
-    assert RequiredParser.call("") == {:error, :required}
     assert RequiredParser.call([]) == {:error, :required}
   end
 end
