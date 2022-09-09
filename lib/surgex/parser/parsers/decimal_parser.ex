e1e008af5c0e100206d721a5438f4339af992b6e
dkuku <daniel.kuku@gmail.com>
2022-09-09 14:31:06 +0100

    add parsing to decimal value

---
+++ lib/surgex/parser/parsers/decimal_parser.ex
@@ -0,0 +1,43 @@
+defmodule Surgex.Parser.DecimalParser do
+  @moduledoc false
+
+  @type errors :: :invalid_decimal | :out_of_range
+  @type option :: {:min, integer()} | {:max, integer()}
+
+  @spec call(term(), [option()]) :: {:ok, Decimal.t() | nil} | {:error, errors()}
+  def call(input, opts \\ [])
+  def call(nil, _opts), do: {:ok, nil}
+  def call("", _opts), do: {:ok, nil}
+
+  def call(input, opts) when is_integer(input) do
+    min = Keyword.get(opts, :min)
+    max = Keyword.get(opts, :max)
+
+    case Decimal.new(input) do
+      :error -> {:error, :invalid_decimal}
+      decimal -> validate_range(decimal, min, max)
+    end
+  end
+
+  def call(input, opts) when is_binary(input) or is_binary(input) do
+    min = Keyword.get(opts, :min)
+    max = Keyword.get(opts, :max)
+
+    case Decimal.parse(input) do
+      :error -> {:error, :invalid_decimal}
+      {decimal, ""} -> validate_range(decimal, min, max)
+      {decimal, _} -> {:error, :invalid_decimal}
+    end
+  end
+
+  def call(_input, _opts), do: {:error, :invalid_decimal}
+
+  defp validate_range(decimal, min, max) do
+    if (is_integer(min) and Decimal.lt?(decimal, min)) or
+         (is_integer(max) and Decimal.gt?(decimal, max)) do
+      {:error, :out_of_range}
+    else
+      {:ok, decimal}
+    end
+  end
+end
---
+++ test/surgex/parser/parsers/decimal_parser_test.exs
@@ -0,0 +1,32 @@
+defmodule Surgex.Parser.DecimalParserTest do
+  use ExUnit.Case
+  alias Surgex.Parser.DecimalParser
+
+  test "nil" do
+    assert DecimalParser.call(nil) == {:ok, nil}
+  end
+
+  test "empty string" do
+    assert DecimalParser.call("") == {:ok, nil}
+  end
+
+  test "valid input" do
+    assert DecimalParser.call(123) == {:ok, Decimal.new("123")}
+    assert DecimalParser.call("123") == {:ok, Decimal.new("123")}
+    assert DecimalParser.call("123", min: 123, max: 123) == {:ok, Decimal.new("123")}
+    assert DecimalParser.call("123.0") == {:ok, Decimal.new("123.0")}
+  end
+
+  test "invalid input" do
+    assert DecimalParser.call("123abc") == {:error, :invalid_decimal}
+    assert DecimalParser.call("?") == {:error, :invalid_decimal}
+    assert DecimalParser.call("1", min: 2) == {:error, :out_of_range}
+    assert DecimalParser.call("1", max: 0) == {:error, :out_of_range}
+  end
+
+  test "unsupported input type" do
+    assert DecimalParser.call(0.5) == {:error, :invalid_decimal}
+    assert DecimalParser.call([1]) == {:error, :invalid_decimal}
+    assert DecimalParser.call(%{test: 1}) == {:error, :invalid_decimal}
+  end
+end
