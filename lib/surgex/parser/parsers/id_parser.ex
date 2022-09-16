3a32249acecc597f33d05c863f127115c87564a2
Hugo <hpjm37@gmail.com>
2022-09-16 12:23:53 +0100

    Set default max for IdParser based on Postgres 4 byte int

--- lib/surgex/parser/parsers/id_parser.ex
+++ lib/surgex/parser/parsers/id_parser.ex
@@ -4,18 +4,26 @@ defmodule Surgex.Parser.IdParser do
   alias Surgex.Parser.IntegerParser
 
   @type errors :: :invalid_identifier | IntegerParser.errors()
+  @type option :: {:max, integer()}
 
-  @spec call(term()) :: {:ok, integer | nil} | {:error, errors}
-  def call(nil), do: {:ok, nil}
-  def call(""), do: {:ok, nil}
+  # This parser already validates that ids cannot be below zero
+  # therefore we don't handle the Postgres min integer case
+  @postgres_max_integer 2_147_483_647
 
-  def call(input) when is_binary(input) do
-    case IntegerParser.call(input) do
+  @spec call(term(), [option()]) :: {:ok, integer | nil} | {:error, errors}
+  def call(input, opts \\ [])
+  def call(nil, _opts), do: {:ok, nil}
+  def call("", _opts), do: {:ok, nil}
+
+  def call(input, opts) when is_binary(input) do
+    max = Keyword.get(opts, :max, @postgres_max_integer)
+
+    case IntegerParser.call(input, max: max) do
       {:ok, integer} when integer > 0 -> {:ok, integer}
       {:ok, _invalid_integer} -> {:error, :invalid_identifier}
       {:error, reason} -> {:error, reason}
     end
   end
 
-  def call(_input), do: {:error, :invalid_identifier}
+  def call(_input, _opts), do: {:error, :invalid_identifier}
 end
--- test/surgex/parser/parsers/id_parser_test.exs
+++ test/surgex/parser/parsers/id_parser_test.exs
@@ -24,4 +24,18 @@ defmodule Surgex.Parser.IdParserTest do
     assert IdParser.call(1.5) == {:error, :invalid_identifier}
     assert IdParser.call(["1"]) == {:error, :invalid_identifier}
   end
+
+  @postgres_max_integer 2_147_483_647
+
+  test "by default enforces max based on 4 byte signed integers" do
+    assert IdParser.call(to_string(@postgres_max_integer)) == {:ok, @postgres_max_integer}
+    assert IdParser.call(to_string(@postgres_max_integer + 1)) == {:error, :out_of_range}
+  end
+
+  test "max is overridable" do
+    new_max = @postgres_max_integer + 1
+
+    assert IdParser.call(to_string(new_max), max: new_max) == {:ok, new_max}
+    assert IdParser.call(to_string(new_max + 1), max: new_max) == {:error, :out_of_range}
+  end
 end
